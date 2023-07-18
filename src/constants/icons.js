3f9b3f89dba47a5f570b347eb6b252630e394873
elilaiono <elilaiono@gmail.com>
2023-07-17 19:21:26 -0600

    opt images

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,15 +1,9 @@
-import barbell from '../assets/barbell.png';
-import benchPress from '../assets/bench-press.jpg'
-import loginImg from '../assets/login-signup-photo.jpg'
-import landingPageImg from '../assets/dead-lift.jpg'
+import landingPageImg from '../assets/dead-lift-opt.jpg'
 import goalImg from '../assets/goals.jpg'
 import wkLogImg from '../assets/wkLog.jpg'
 import logo from '../assets/dumbbell.png'
 
 const icons = {
-  barbell,
-  benchPress,
-  loginImg,
   landingPageImg,
   goalImg,
   wkLogImg,
